0cd6b6b586bdf3dda55cccbf7a495303a5cabdae
Isa Ozler <contactme@isaozler.com>
2021-11-19 19:17:31 +0100

    hotfix import React

--- src/components/molecules/Columns/component.tsx
+++ src/components/molecules/Columns/component.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useRef, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import GlobalStyle from '@components/globalStyle';
 
 import IProps from './schema';
